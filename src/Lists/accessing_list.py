c08be2bc935644f910717b3b5c2a24dea70c1d1c
Sourabh Jadhav <sourabhjadha2222@gmail.com>
2022-06-15 22:42:20 +0530

    added list examples

---
+++ src/Lists/accessing_list.py
@@ -0,0 +1,14 @@
+# List is declared in square bracket
+
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+print(len(mylist))
+
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[1])
+print(thislist[-1])
+print(thislist[2:5])
+print(thislist[:4])
+print(thislist[2:])
+print(thislist[-4:-1])
